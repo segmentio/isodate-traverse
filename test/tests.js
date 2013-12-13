bc6c564a2c5b5ca1d375040233491f0685029a48
Dominic Barnes <dbarnes@treetopllc.com>
2013-12-13 11:09:52 -0600

    adding array support

--- test/tests.js
+++ test/tests.js
@@ -23,11 +23,49 @@ it('should convert numbers if strict mode is disabled', function () {
   assert(2011 == traversed.date.getFullYear());
 });
 
-it('should return a clone', function () {
+it('should return a clone object', function () {
   var obj = { a: '2' };
   var traversed = traverse(obj);
   assert(obj != traversed);
   assert(equal({ a: '2' }, traversed));
+});
+
+it('should iterate through arrays', function () {
+  var arr = [
+    { date: '2013-09-04T00:57:26.434Z' }
+  ];
+
+  var traversed = traverse(arr);
+  assert(arr != traversed);
+  assert('2013-09-04T00:57:26.434Z' == traversed[0].date.toISOString());
+});
+
+it('should iterate through nested arrays', function () {
+  var arr = [
+    {
+      date: '2013-09-04T00:57:26.434Z',
+      array: [
+        { date: '2013-09-04T00:57:26.434Z' }
+      ]
+    }
+  ];
+
+  var traversed = traverse(arr);
+  assert(arr != traversed);
+  assert('2013-09-04T00:57:26.434Z' == traversed[0].array[0].date.toISOString());
+});
+
+it('should propagate the "strict" parameter for both types of traversals', function () {
+  var obj = {
+    a: '2012',
+    b: [
+      { c: '2012' }
+    ]
+  };
+
+  var traversed = traverse(obj, false);
+  assert(equal(2011, traversed.a.getFullYear()));
+  assert(equal(2011, traversed.b[0].c.getFullYear()));
 });
 
 });
